1d385bed34228a7354180d2047c860cae15e3650
slydoge <atomicagression@gmail.com>
2018-07-04 07:18:21 +0300

    fix14

--- testBot.js
+++ testBot.js
@@ -6,10 +6,10 @@ const app = express().use(bodyParser.json());
 
 const config = require("./config.json");
 
-const config = require('./messageHandler.js');
+const messageHandler = require('./messageHandler.js');
 const handler = new messageHandler(app, config.PAGE_ACCESS_TOKEN);
 
-messageHandler.on('message', function(data) {
+handler.on('message', function(data) {
     let response;
     let randomNumber = Math.floor((Math.random() * 10000) + 1);
 
@@ -20,9 +20,9 @@ messageHandler.on('message', function(data) {
         default:
             response = {"text": 'type abracadabra if you want to see the magic number'};
     }
-    messageHandler.send(data.sender, response);
+    handler.send(data.sender, response);
 });
 
 app.listen(8085, () => console.log('webhook is listening'));
 
-messageHandler.castHook(config.VERIFY_TOKEN);
+handler.castHook(config.VERIFY_TOKEN);
