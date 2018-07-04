d6eef3aa50bfb6891131401c0733e04b74b499df
slydoge <atomicagression@gmail.com>
2018-07-04 08:02:29 +0300

    techie

--- messageHandler.js
+++ messageHandler.js
@@ -33,42 +33,11 @@ class messageHandler extends EventEmitter {
              handlePostback(sender_psid, webhook_event.postback);
           }
         });
-    res.status(200).send('EVENT_RECEIVED');
-    } else {
-        res.sendStatus(404);
-    }
-  });
-
-/*
-    console.log("Hook pulled");
-    this.app.post('/webhook', (req, res) => {
-      console.log("Webhook received a post");
-      let body = req.body;
-      if (body.object === 'page') {
-          body.entry.forEach((entry) => {
-              let webhook_event = entry.messaging[0];
-              console.log(webhook_event);
-
-              let sender_psid = webhook_event.sender.id;
-              console.log('Sender PSID: ' + sender_psid);
-
-              if (webhook_event.message) {
-                //emit message event
-                var data = {
-                  sender: sender_psid, 
-                  message : webhook_event.message
-                }
-                this.emit('message', data);
-              } 
-              else if (webhook_event.postback) handlePostback(sender_psid, webhook_event.postback);
-          });
-
       res.status(200).send('EVENT_RECEIVED');
-      } 
-      else res.sendStatus(404);
-
+      } else {
+          res.sendStatus(404);
+      }
     });
-    */
   }
   castHook(VERIFY_TOKEN){
     this.app.get('/webhook', (req, res) => {
